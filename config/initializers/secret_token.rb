b1a72a70650a45bf0afccfbe20b7115ba75328d5
Chris Malin <csmalin@gmail.com>
2013-04-27 12:00:38 -0700

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pseudochat::Application.config.secret_token = '05f082f42c548161f62cc08a7d0e8061b23d7fff8164e4eefb25ddc931aaacd1c721f52832cbe220b25d00482d21095dc1cc667157bd51acde6281495d8989ee'
