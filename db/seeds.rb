3b6327d09502741c6395ebecdf8690e15eb0d2ff
Chris Malin <csmalin@gmail.com>
2013-04-27 17:14:39 -0700

    Chats & Messages, migrations & models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+u1 = User.create(:name => "Chris Malin", :email => "csmalin@gmail.com", :password => "password")
+u2 = User.create(:name => "Philip Woo", :email => "pwoo@gmail.com", :password => "password")
+
+chat = Chat.create(title:"Test Chat")
+
+chat.messages.build(text:"user1 text", user_id:u1.id).save
+chat.messages.build(text:"user2 text", user_id:u2.id).save
+
